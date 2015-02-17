b6f374b34d4741772c18a61267d1cca32e716aba
sappon <yangliu378@gmail.com>
2015-02-17 23:20:28 +0800

    added addAccountType and getAll accountType; updateAccountType has 500 error

--- api/accountType.js
+++ api/accountType.js
@@ -2,19 +2,26 @@ require('./config');
 var utils = require('./utils');
 var request = require('request');
 
-exports.getAccountType = function(id, callback) {
-	var url = 'https://leancloud.cn/1.1/classes/accountType/' + id;
+var urlRoot = 'https://leancloud.cn/1.1/classes';
+var className = '/accountType';
+
+exports.getById= function(id, callback) {
+	var url = urlRoot + className + id;
 	//console.log(url);
 	var options = utils.getOptions(url);
 	// console.log(options);
 	request.get(options, callback);
 }
 
-exports.addAccountType = function(id, callback) {
-    var url = 'https://leancloud.cn/1.1/classes/accountType';
-    var data = JSON.stringify({
-        "typeCode" : "md's test"
-    });
+exports.getAll = function(callback) {
+    var url = urlRoot + className;
+    console.log(url);
+    var options = utils.getOptions(url);
+    request.get(options, callback);
+}
+
+exports.add = function(data, callback) {
+    var url = urlRoot + className; 
     var options = utils.getOptions(url);
     options.body = data;
     request.post(options, callback);
@@ -25,5 +32,11 @@ exports.addAccountType = function(id, callback) {
     // // attach data to request body
     // req.write(data);
     // req.end();
-    
+}
+
+exports.updateById = function(id, data, callback) {
+    var url = urlRoot + className + id;
+    var options = utils.getOptions(url);
+    options.body = data;
+    request.put(options, callback);
 }
--- routes/test.js
+++ routes/test.js
@@ -1,13 +1,12 @@
 var express = require('express');
-var router = express.Router();
-var accountType = require('../api/accountType');
+var router = express.Router(); var accountType = require('../api/accountType');
 
-/* test get accountType */
-router.get('/getAccountType/:id', function(req, res, next) {
-    var id = req.params.id;
+/* test get accountType by ID */
+router.get('/getAccType/:id', function(req, res, next) {
+    var id = '/' + req.params.id;
     //console.log(id);
     
-    accountType.getAccountType(id, function (error, response, body) {
+    accountType.getById(id, function (error, response, body) {
         if(!error && response.statusCode == 200){
             res.send(body);
         } else {
@@ -16,17 +15,43 @@ router.get('/getAccountType/:id', function(req, res, next) {
     });
 });
 
+/* test get all accountType */
+router.get('/getAllAccType', function(req, res, next) {
+    accountType.getAll(function(error, response, body) {
+        if(!error){
+            res.send(body);
+        } else {
+            res.send(error);
+        }
+    });
+});
+
 /* test add accountType */
-router.get('/addAccountType', function(req, res, next) {
+router.get('/addAccType', function(req, res, next) {
     var data = JSON.stringify({
         "typeCode" : "aaa"
     });
 
-    accountType.addAccountType(data, function(error, response, body){
+    accountType.add(data, function(error, response, body){
         if(!error && response.statusCode == 201){
             res.send("body: " + body);
         } else {
             res.send("error: " + error); 
+        }
+    });
+});
+
+/* test update accountType */
+router.get('/updateAccType/:id', function(req, res, next) {
+    var data = JSON.stringify({
+        "typeCode" : "changed"
+    });
+
+    accountType.updateById(id, data, function(error, response, body){
+        if(!error){
+            res.send(body);
+        } else {
+            res.send(error);
         }
     });
 });
