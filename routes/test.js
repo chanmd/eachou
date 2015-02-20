9fa28e748f5527449bd54bbb338a9e50b1e9730a
sappon <yangliu378@gmail.com>
2015-02-20 20:11:13 +0800

    added ngo basic APIs, including: getAll, getById, add, updateById, deleteById

--- routes/test.js
+++ routes/test.js
@@ -1,5 +1,9 @@
 var express = require('express');
-var router = express.Router(); var accountType = require('../api/accountType');
+var router = express.Router();
+
+/*------------------- accountType -----------------------*/
+ 
+var accountType = require('../api/accountType');
 
 /* test get accountType by ID */
 router.get('/getAccType/:id', function(req, res, next) {
@@ -17,7 +21,7 @@ router.get('/getAccType/:id', function(req, res, next) {
 
 /* test get all accountType */
 router.get('/getAllAccType', function(req, res, next) {
-    accountType.getAll(function(error, response, body) {
+    accountType.getAll(function(error, response, body) { 
         if(!error){
             res.send(body);
         } else {
@@ -62,6 +66,83 @@ router.get('/deleteAccType/:id', function(req, res, next) {
     var id = '/' + req.params.id;
     
     accountType.deleteById(id, function(error, response, body){
+        if(!error){
+            res.send(body);
+        } else {
+            res.send(error);
+        }
+    });
+});
+
+
+/*------------------- ngo -----------------------*/
+ 
+var ngo = require('../api/ngo');
+
+/* test get ngo by ID */
+router.get('/getNgo/:id', function(req, res, next) {
+    var id = '/' + req.params.id;
+    //console.log(id);
+    
+    ngo.getById(id, function (error, response, body) {
+        if(!error && response.statusCode == 200){
+            res.send(body);
+        } else {
+            res.send(error); 
+        }
+    });
+});
+
+/* test get all ngo */
+router.get('/getAllNgo', function(req, res, next) {
+    ngo.getAll(function(error, response, body) { 
+        if(!error){
+            res.send(body);
+        } else {
+            res.send(error);
+        }
+    });
+});
+
+/* test add ngo */
+router.get('/addNgo', function(req, res, next) {
+    var data = JSON.stringify({
+        "name" : "儿童基金",
+        "mission" : "让每个孩子都有读书的机会",
+        "tel" : 98765432
+    });
+
+    ngo.add(data, function(error, response, body){
+        if(!error && response.statusCode == 201){
+            res.send("body: " + body);
+        } else {
+            res.send("error: " + error); 
+        }
+    });
+});
+
+/* test update ngo */
+router.get('/updateNgo/:id', function(req, res, next) {
+    var id = '/' + req.params.id;
+    var data = JSON.stringify({
+        "tel" : 000,
+        "location" : "中国辽宁省沈阳市"
+    });
+
+    ngo.updateById(id, data, function(error, response, body){
+        if(!error){
+            res.send(body);
+        } else {
+            res.send(error);
+        }
+    });
+});
+
+/* test delete accountType */
+router.get('/deleteNgo/:id', function(req, res, next) {
+    var id = '/' + req.params.id;
+    
+    ngo.deleteById(id, function(error, response, body){
         if(!error){
             res.send(body);
         } else {
